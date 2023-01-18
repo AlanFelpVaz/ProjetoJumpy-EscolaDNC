d0259bf484589ccbe3f2e6cb4d137dde63cca6fa
Alan Felipe <alanf.e1601@gmail.com>
2023-01-18 12:36:53 -0300

    detalhes e media query

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+
+//a maior gambiarra ja feita
+
+
 function verificar(){
 
+    //aqui pega todos elementos necessarios
 
     let nome = document.getElementById("nome")
     let email = document.getElementById("email")
@@ -8,6 +13,8 @@ function verificar(){
     let senha = document.getElementById("senha")
 
     const btnSpan = document.querySelector('#erroBtn')
+
+    // aqui remove a mensagem de erro
 
     let msgErro = document.querySelectorAll('form .cadastro');
 
@@ -18,6 +25,9 @@ function verificar(){
         document.querySelector('#erro4').textContent = ``;
         document.querySelector('#erro5').textContent = ``;
     }
+
+    // aqui adiciona a mensagem de erro se os espaços estiverem vazios
+    // vai passando cada input por vez, não do jeito que eu queria, mas funciona
 
     if (nome.value == '') {
         document.querySelector('#erro1').textContent = `'Campo Obrigatório'`;
@@ -51,7 +61,9 @@ function verificar(){
         document.querySelector('#erroBtn').textContent = `Sucesso!`
     }
 
-    
+    // em cima adiciona a mensagem de sucesso caso todos inputs estejam com algum texto
 
+    // usei classes para fazer a mensagem de erro sumir e aparecer, usando classlist .add e .remove
+    // sei que poderia ter feito de maneira mais facíl e melhor, irei melhorar
 
 }
